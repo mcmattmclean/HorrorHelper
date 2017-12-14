76697b4075a320bdfb6eccf330ee6ddff6c5b195
Matthew McLean <matthewmclean21@gmail.com>
2017-12-14 12:21:26 -0800

    Add specific buttons and steppers

--- HorrorHelper/ViewController.swift
+++ HorrorHelper/ViewController.swift
@@ -20,30 +20,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var phaseHeaderLabel: UILabel!
     @IBOutlet weak var descriptionField: UILabel!
+    @IBOutlet weak var nextPhaseButton: UIButton!
+    @IBOutlet weak var previousPhaseButton: UIButton!
     
-    @IBOutlet weak var setTextButton: UIButton!
+    @IBOutlet weak var gateOpenStepper: UIStepper!
+    @IBOutlet weak var gateSealedStepper: UIStepper!
+    @IBOutlet weak var monsterTownStepper: UIStepper!
+    @IBOutlet weak var monsterOutskirtStepper: UIStepper!
+    @IBOutlet weak var terrorTrackStepper: UIStepper!
     
-    @IBAction func updateText(_ sender: Any) {
-        descriptionField.text = "1. Refresh exhausted cards.\n\n2. Perform upkeep actions:\n\tBless\n\tCurse\n\tBank Loan\n\tRetainer\n\n3. Adjust skills within character's focus stat."
+    @IBOutlet weak var gateOpenLabel: UILabel!
+    @IBOutlet weak var gateSealedLabel: UILabel!
+    @IBOutlet weak var monsterTownLabel: UILabel!
+    @IBOutlet weak var monsterOutskirtLabel: UILabel!
+    @IBOutlet weak var terrorTrackLabel: UILabel!
+    
+    @IBAction func gateOpenChanged(_ sender: UIStepper) {
+        gateOpenLabel.text = String(sender.value)
     }
-    
-    @IBOutlet weak var hollyButton: UIButton!
-    @IBAction func textOut(_ sender: Any) {
-        descriptionField.text = "bob ger sux"
+    @IBAction func gateSealedChanged(_ sender: UIStepper) {
+        gateSealedLabel.text = String(sender.value)
     }
-    
-    @IBOutlet weak var stepper: UIStepper!
-    
-    @IBOutlet weak var stepVal: UILabel!
-    
-    @IBAction func changeStepper(_ sender: Any) {
-        stepVal.text = String(stepper.value)
-        if(stepper.value > 6){
-//            descriptionField.text = "You've won!"
-//            descriptionField.textColor = UIColor.red
-        }
+    @IBAction func monsterTownChanged(_ sender: UIStepper) {
+        monsterTownLabel.text = String(sender.value)
     }
-    
+    @IBAction func monsterOutskirtChanged(_ sender: UIStepper) {
+        monsterOutskirtLabel.text = String(sender.value)
+    }
+    @IBAction func terrorTrackChanged(_ sender: UIStepper) {
+        terrorTrackLabel.text = String(sender.value)
+    }
 }
 
