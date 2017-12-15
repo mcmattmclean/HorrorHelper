a373f8e37274d051687ee0de3ef36ec0d1c885ac
Matthew McLean <matthewmclean21@gmail.com>
2017-12-14 17:36:58 -0800

    Add next, previous, and alert functionality

--- HorrorHelper/GameState.swift
+++ HorrorHelper/GameState.swift
@@ -9,20 +9,26 @@
 import Foundation
 
 class GameState {
-    private var players: Int
+    var players: Int
+    var phaseHeader: String?
     var currentMessage: String?
+    private var currentPhase: Phase
+    private var oldPhase: Phase
     
-    private var monstersTown: Int
-    private var monstersOutskirts: Int
-    private var gatesOpen: Int
-    private var gatesSealed: Int
+    var monstersTown: Int
+    var monstersOutskirts: Int
+    var gatesOpen: Int
+    var gatesSealed: Int
     
     let maximumGatesSealed: Int = 6
     
     private var maximumMonstersOutskirts: Int
+    private var maximumMonstersTown: Int
     private var maximumGatesOpen: Int
     
     private enum Phase {
+        case start
+        case alert
         case upkeep
         case movement
         case arkhamEncounters
@@ -30,15 +36,61 @@ class GameState {
         case mythos
     }
     
+    private func updatePhaseString() {
+        switch currentPhase {
+        case .upkeep:
+            currentMessage = "Do upkeep stuff"
+            phaseHeader = "Upkeep"
+        case .movement:
+            currentMessage = "Do movement stuff"
+            phaseHeader = "Movement"
+        case .arkhamEncounters:
+            currentMessage = "Do arkham encounters stuff"
+            phaseHeader = "Arkham Encounters"
+        case .otherworldEncounters:
+            currentMessage = "Otherworld stuff"
+            phaseHeader = "Otherworld Encounters"
+        case .mythos:
+            currentMessage = "Do mythos stuff"
+            phaseHeader = "Mythos"
+        case .start:
+            currentMessage = "Please adjust the number of players to get started. Then, press next when you're ready to begin the game."
+            phaseHeader = "Welcome to Horror Helper!"
+        case .alert:
+            break
+        }
+    }
+    
     init() {
         players = 0
-        currentMessage = "Welcome to Horror Helper! Please adjust the number of players to get started. Then, press next when you're ready to begin the game."
+        currentMessage = ""
+        phaseHeader = ""
         monstersTown = 0
+        currentPhase = .start
+        oldPhase = .start
         monstersOutskirts = 0
         gatesOpen = 0
         gatesSealed = 0
         maximumMonstersOutskirts = 100
+        maximumMonstersTown = 100
         maximumGatesOpen = 100
+        updatePhaseString()
+    }
+    
+    public func reset() {
+        players = 0
+        currentMessage = "Game reset"
+        phaseHeader = ""
+        monstersTown = 0
+        currentPhase = .start
+        oldPhase = .start
+        monstersOutskirts = 0
+        gatesOpen = 0
+        gatesSealed = 0
+        maximumMonstersOutskirts = 100
+        maximumMonstersTown = 100
+        maximumGatesOpen = 100
+        updatePhaseString()
     }
     
     public func setGameConstraints(numPlayers: Int) {
@@ -56,11 +108,107 @@ class GameState {
             maximumGatesOpen = 5
         }
         maximumMonstersOutskirts = 8 - numPlayers
+        maximumMonstersTown = numPlayers + 3
     }
     
     public func changeGatesOpen(newNumGates: Int) {
         if newNumGates >= maximumGatesOpen {
             currentMessage = "The Ancient One awakens! Please enter combat at your earliest convenience."
+            phaseHeader = "Uh oh!"
+            currentPhase = .alert
+            updatePhaseString()
         }
+        else {
+            gatesOpen = newNumGates
+        }
+    }
+    
+    public func changeGatesSealed(newNumGates: Int) {
+        if newNumGates >= maximumGatesSealed {
+            currentMessage = "You've sealed 6 gates! The Ancient One has been defeated... this time..."
+            currentPhase = .alert
+            phaseHeader = "Game Over"
+            updatePhaseString()
+        }
+        else {
+            gatesSealed = newNumGates
+        }
+    }
+    
+    public func changeMonstersTown(newNumMonsters: Int) {
+        // Handle monsters over town limit
+        if newNumMonsters > maximumMonstersTown {
+            currentMessage = "Monster limit reached! Monster goes to outskirts."
+            monstersOutskirts += 1
+            phaseHeader = "Monsters are everywhere!"
+            
+            // Handle monsters over outskirts limit
+            if monstersOutskirts > maximumMonstersOutskirts {
+                currentMessage = "Monsters have flooded the outskirts! Empty the outskirts and raise the terror level by one!"
+                phaseHeader = "Terror grips Arkham!"
+                monstersOutskirts = 0
+            }
+            currentPhase = .alert
+            updatePhaseString()
+        }
+        else {
+            monstersTown = newNumMonsters
+        }
+        
+    }
+    
+    public func advancePhase () {
+        switch currentPhase {
+        case .start:
+            currentPhase = .upkeep
+            updatePhaseString()
+        case .upkeep:
+            currentPhase = .movement
+            updatePhaseString()
+        case .movement:
+            currentPhase = .arkhamEncounters
+            updatePhaseString()
+        case .arkhamEncounters:
+            currentPhase = .otherworldEncounters
+            updatePhaseString()
+        case .otherworldEncounters:
+            currentPhase = .mythos
+            updatePhaseString()
+        case .mythos:
+            currentPhase = .upkeep
+            updatePhaseString()
+        case .alert:
+            currentPhase = oldPhase //Get phase from before alert and reset to that phase
+            goBackPhase()
+            advancePhase()
+        }
+        oldPhase = currentPhase
+    }
+    
+    public func goBackPhase () {
+        switch currentPhase {
+        case .start:
+            break
+        case .upkeep:
+            currentPhase = .mythos
+            updatePhaseString()
+        case .movement:
+            currentPhase = .upkeep
+            updatePhaseString()
+        case .arkhamEncounters:
+            currentPhase = .movement
+            updatePhaseString()
+        case .otherworldEncounters:
+            currentPhase = .arkhamEncounters
+            updatePhaseString()
+        case .mythos:
+            currentPhase = .otherworldEncounters
+            updatePhaseString()
+        default:
+            currentPhase = oldPhase
+            advancePhase()
+            goBackPhase()
+        }
+        oldPhase = currentPhase
     }
 }
--- HorrorHelper/ViewController.swift
+++ HorrorHelper/ViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var gateOpenStepper: UIStepper!
     @IBOutlet weak var gateSealedStepper: UIStepper!
     @IBOutlet weak var monsterTownStepper: UIStepper!
-    @IBOutlet weak var monsterOutskirtStepper: UIStepper!
     @IBOutlet weak var numberPlayersStepper: UIStepper!
     
     @IBOutlet weak var gateOpenLabel: UILabel!
@@ -40,22 +39,59 @@ class ViewController: UIViewController {
     
     private var game: GameState = GameState()
     
+    private func updateDescription(){
+        descriptionField.text = game.currentMessage
+        phaseHeaderLabel.text = game.phaseHeader
+    }
+    
+    @IBAction func resetGame(_ sender: UIButton) {
+        game.reset()
+        updateDescription()
+        gateSealedLabel.text = "0"
+        gateSealedStepper.value = 0
+        gateOpenLabel.text = "0"
+        gateOpenStepper.value = 0
+        monsterTownLabel.text = "0"
+        monsterTownStepper.value = 0
+        monsterOutskirtLabel.text = "0"
+        numberPlayersLabel.text = "0"
+        numberPlayersStepper.value = 0
+    }
+    
+    @IBAction func nextPhase(_ sender: UIButton) {
+        game.advancePhase()
+        updateDescription()
+    }
+    
+    @IBAction func revertPhase(_ sender: UIButton) {
+        game.goBackPhase()
+        updateDescription()
+    }
+    
     @IBAction func gateOpenChanged(_ sender: UIStepper) {
-//        gateOpenLabel.text = String(Int(sender.value))
         game.changeGatesOpen(newNumGates: Int(sender.value))
+        gateOpenLabel.text = String(game.gatesOpen)
+        gateOpenStepper.value = Double(game.gatesOpen)
+        updateDescription()
     }
     @IBAction func gateSealedChanged(_ sender: UIStepper) {
-//        gateSealedLabel.text = String(Int(sender.value))
+        game.changeGatesSealed(newNumGates: Int(sender.value))
+        gateSealedLabel.text = String(game.gatesSealed)
+        gateSealedStepper.value = Double(game.gatesSealed)
+        updateDescription()
     }
     @IBAction func monsterTownChanged(_ sender: UIStepper) {
-//        monsterTownLabel.text = String(Int(sender.value))
+        game.changeMonstersTown(newNumMonsters: Int(sender.value))
+        monsterTownLabel.text = String(game.monstersTown)
+        monsterOutskirtLabel.text = String(game.monstersOutskirts)
+        monsterTownStepper.value = Double(game.monstersTown)
+        updateDescription()
     }
-    @IBAction func monsterOutskirtChanged(_ sender: UIStepper) {
-//        monsterOutskirtLabel.text = String(Int(sender.value))
-    }
+    
     @IBAction func numberPlayersChanged(_ sender: UIStepper) {
 //        numberPlayersLabel.text = String(Int(sender.value))
         game.setGameConstraints(numPlayers: Int(sender.value))
+        numberPlayersLabel.text = String(game.players)
     }
 }
 
